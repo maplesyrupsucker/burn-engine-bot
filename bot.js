73d69ce76924af60d627cb657eee0e332b04e600
Corbin <corbinfraser@gmail.com>
2024-02-16 18:58:33 +0700

    x

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const Web3 = require("web3");
 const axios = require("axios");
-const { handleTelegramPost, handleTelegramCommand, notifyError } = require("./handlers/telegramHandler");
+const { handleTelegramPost, handleTelegramCommands, notifyError } = require("./handlers/telegramHandler");
 const { handleSlackPost } = require("./handlers/slackHandler");
 // const { handleFacebookPost } = require("./handlers/facebookHandler");
 // const { handleDiscordPost } = require("./handlers/discordHandler");
@@ -257,7 +257,7 @@ async function initialize() {
     monitorEvents();
 
     // Initialize Telegram commands
-    handleTelegramCommand();
+    handleTelegramCommands();
     setupTelegramCommands({
       verseTokenContract,
       fetchLastFiveBurns,
