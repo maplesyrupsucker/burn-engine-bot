63bbd6a51b68cc8f5f49159c959b5c69188b97ba
Corbin <corbinfraser@gmail.com>
2025-01-02 14:59:10 +0700

    x

--- bot.js
+++ bot.js
@@ -28,6 +28,7 @@ let lastTelegramNotificationTime = 0;
 let verseUsdRate = 0;
 let lastProcessedBlock = 0;
 let lastKnownBalanceEth = 0;
+let lastTelegramNotificationTime = 0; // Initialize the variable to track Telegram notification time
 let lastReportedTelegramBalance = ""; // Initialize the variable to track the last reported balance on Telegram
 let totalBuybacksEth = 0;
 let totalBuybacksUsd = 0;
